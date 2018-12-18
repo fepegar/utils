2fd1efa9efce4f099b7ac20ac47771e69ddad6b8
Fernando Perez-Garcia <fepegar@gmail.com>
2018-12-18 09:28:22 +0000

    Accept multiple paths to be ensured

--- utils/path.py
+++ utils/path.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+from collections import Iterable
 
-def ensure_dir(path):
+def ensure_dir(paths):
     """Make sure that the directory and its parents exists"""
-    path = Path(path)
-    if path.exists():
-        return
-    is_dir = not path.suffixes
-    if is_dir:
-        path.mkdir(parents=True, exist_ok=True)
-    else:
-        path.parent.mkdir(parents=True, exist_ok=True)
+    if not isinstance(paths, Iterable):
+        paths = [paths]
+    for path in paths:
+        path = Path(path)
+        if path.exists():
+            return
+        is_dir = not path.suffixes
+        if is_dir:
+            path.mkdir(parents=True, exist_ok=True)
+        else:
+            path.parent.mkdir(parents=True, exist_ok=True)
