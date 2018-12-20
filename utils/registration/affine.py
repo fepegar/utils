4a5d6e32e66f212e6b12cb08a201efd5b7c40cc6
Fernando Perez-Garcia <fepegar@gmail.com>
2018-12-20 14:47:13 +0000

    Add registration module

---
+++ utils/registration/affine.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+from os.path import isfile, splitext
+
+import numpy as np
+
+from . import io
+
+
+VALID_TRSF_EXTENSIONS = io.NIFTYREG_EXT, io.ITK_EXT
+
+
+
+class AffineMatrix(object):
+    """Stores an affine matrix going from floating to reference"""
+
+    def __init__(self, affine=None, invert=False):
+        """The input parameter affine can be a path or an array"""
+        self.matrix = np.eye(4)
+
+        if affine is None:
+            return
+
+        if isinstance(affine, (str, Path)):  # path
+            path = str(affine)
+            self.matrix = self.read(path)
+
+        elif isinstance(affine, np.ndarray):
+            if affine.shape != (4, 4):
+                raise ValueError('Input array must have shape (4, 4), '
+                                 'not {}'.format(affine.shape))
+            self.matrix = affine.astype(np.float)
+
+        elif isinstance(affine, AffineMatrix):
+            self.matrix = affine.matrix
+
+        else:
+            raise ValueError('Input must be a path, '
+                             'an AffineMatrix or an array')
+
+        if invert:
+            self.invert()
+
+
+    def __repr__(self):
+        np.set_printoptions(precision=2, suppress=True)
+        string = '{}\n{}'.format(self.__class__.__name__,
+                                 str(self.matrix))
+        return string
+
+
+    def __mul__(self, other):
+        result = AffineMatrix(self)
+        result.right_multiply(other)
+        return result
+
+
+    @property
+    def inverse(self):
+        return AffineMatrix(np.linalg.inv(self.matrix))
+
+
+    def invert(self):
+        self.matrix = np.linalg.inv(self.matrix)
+
+
+    def parse_path(self, path):
+        if not isfile(path):
+            raise FileNotFoundError('The transform {} '
+                                    'does not exist'.format(path))
+        self.parse_extension(path)
+
+
+    def parse_extension(self, path):
+        _, extension = splitext(path)
+        if extension not in VALID_TRSF_EXTENSIONS:
+            raise IOError('Transform extension must be .txt or .tfm, '
+                          'not {}'.format(extension))
+
+
+    def read(self, path):
+        self.parse_path(path)
+        if path.endswith(io.NIFTYREG_EXT):  # NiftyReg
+            matrix = io.read_niftyreg_matrix(path)
+        elif path.endswith(io.ITK_EXT):
+            matrix = io.read_itk_matrix(path)
+        return matrix
+
+
+    def write(self, path):
+        self.parse_extension(path)
+        if path.endswith(io.NIFTYREG_EXT):  # NiftyReg
+            io.write_niftyreg_matrix(self.matrix, path)
+        elif path.endswith(io.ITK_EXT):
+            io.write_itk_matrix(self.matrix, path)
+
+
+    def left_multiply(self, affine):
+        affine = AffineMatrix(affine)
+        self.matrix = np.dot(affine.matrix, self.matrix)
+
+
+    def right_multiply(self, affine):
+        affine = AffineMatrix(affine)
+        self.matrix = np.dot(self.matrix, affine.matrix)
---
+++ utils/registration/io.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+FLIPXY = np.diag([-1, -1, 1, 1])
+ITK_EXT = '.tfm'
+NIFTYREG_EXT = '.txt'
+
+
+def read_niftyreg_matrix(trsf_path):
+    """Read a NiftyReg matrix and return it as a NumPy array"""
+    matrix = np.loadtxt(trsf_path)
+    matrix = np.linalg.inv(matrix)
+    return matrix
+
+
+def write_niftyreg_matrix(matrix, txt_path):
+    """Write an affine transform in NiftyReg's .txt format (ref -> flo)"""
+    matrix = np.linalg.inv(matrix)
+    np.savetxt(txt_path, matrix, fmt='%.8f')
+
+
+def to_itk_convention(matrix):
+    """Apply some operations so that the transform is in ITK's LPS format"""
+    matrix = np.dot(FLIPXY, matrix)
+    matrix = np.dot(matrix, FLIPXY)
+    matrix = np.linalg.inv(matrix)
+    return matrix
+
+
+def from_itk_convention(matrix):
+    """From floating to reference, LPS"""
+    matrix = np.dot(matrix, FLIPXY)
+    matrix = np.dot(FLIPXY, matrix)
+    matrix = np.linalg.inv(matrix)
+    return matrix
+
+
+def read_itk_matrix(trsf_tfm_path):
+    """Read an affine transform in ITK's .tfm format"""
+    import SimpleITK as sitk
+    transform = sitk.ReadTransform(trsf_tfm_path)
+    parameters = transform.GetParameters()
+
+    rotation_parameters = parameters[:9]
+    rotation_matrix = np.array(rotation_parameters).reshape(3, 3)
+
+    translation_parameters = parameters[9:]
+    translation_vector = np.array(translation_parameters).reshape(3, 1)
+
+    matrix = np.hstack([rotation_matrix, translation_vector])
+    homogeneous_matrix_lps = np.vstack([matrix, [0, 0, 0, 1]])
+    homogeneous_matrix_ras = from_itk_convention(homogeneous_matrix_lps)
+    return homogeneous_matrix_ras
+
+
+def write_itk_matrix(matrix, tfm_path):
+    """The tfm file contains the matrix from floating to reference."""
+    import SimpleITK as sitk
+    matrix = to_itk_convention(matrix)
+    rotation = matrix[:3, :3].ravel().tolist()
+    translation = matrix[:3, 3].tolist()
+    transform = sitk.AffineTransform(rotation, translation)
+    transform.WriteTransform(tfm_path)
